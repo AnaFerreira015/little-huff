faf193b1c8092f04b4fbdfb170a6543cacabcb7f
anaferreira015 <dasilvaferreira.af@hotmail.com>
2019-06-13 23:06:33 -0300

    Creates the documentation

--- frequency.c
+++ frequency.c
@@ -4,8 +4,14 @@
 
 #define MAX_SIZE 256
 
+/**
+ * - Uses unsigned char to preserve the pattern and the most significant bit to lose its function as signal bit
+ */
 typedef unsigned char BYTE;
 
+/**
+ * The function receives an array, its size and an index, and returns an array initialized with 0's
+ */
 void initialize_array(int *array, int size, int i)
 {
     if (i < size)
@@ -16,10 +22,15 @@ void initialize_array(int *array, int size, int i)
     }
 }
 
+/**
+* The function receives the file name and returns an array with the frequency of each byte in the file
+ */
 int *create_frequency_array(char arq[])
 {
+    /** Opens the file in binary read mode */
     FILE *file = fopen(arq, "rb");
 
+    /** Creates an initialized array for the frequency */
     int *freq = (int *)calloc(MAX_SIZE, sizeof(int));
 
     // initialize_array(freq, MAX_SIZE, 0);
@@ -29,6 +40,7 @@ int *create_frequency_array(char arq[])
     {
         if (!feof(file))
         {
+            /** It takes each byte of the array and stores its frequency in the array. */
             BYTE c = fgetc(file);
             i = (int)c;
             freq[i] += 1;
@@ -42,6 +54,9 @@ int *create_frequency_array(char arq[])
     return freq;
 }
 
+/**
+ * Receive an array already created and display what's in it
+ */
 void printing_frequency_array(int *array)
 {
     int i;
@@ -49,6 +64,7 @@ void printing_frequency_array(int *array)
     {
         if (array[i])
         {
+            /** Displays character and frequency */
             printf("%c %d\n", i, array[i]);
         }
     }
