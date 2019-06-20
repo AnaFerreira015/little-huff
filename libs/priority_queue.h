1891f398086d86327ff55c7f1e6c4f1ba1a07433
Kelly <kellbianca0@gmail.com>
2019-06-20 15:02:15 -0300

    extern nas assinaturas dos arquivos

--- libs/priority_queue.h
+++ libs/priority_queue.h
@@ -14,26 +14,26 @@ typedef struct priority_queue
 } priority_queue;
 
 /** Checks if queue is empty */
-int isEmpty(priority_queue *PQ);
+extern int isEmpty(priority_queue *PQ);
 
 /** Creates a new cell for the priority queue and returns it */
-NODE *creating_element(char character, int frequency);
+extern NODE *creating_element(char character, int frequency);
 
 /**
  * - You receive a cell from the priority queue and the data to add to it
  * - Add the parameter data in the queue
  */
-void enqueue(priority_queue *PQ, char character, int frequency);
+extern void enqueue(priority_queue *PQ, char character, int frequency);
 /**
  * - Receives a cell from the priority queue and removes it from the queue
  * - Returns the value taken
  */
-NODE *dequeue(priority_queue *PQ);
+extern NODE *dequeue(priority_queue *PQ);
 
 /** Receives a priority queue and returns the highest value (frequency) of the queue */
-int maximum(priority_queue *PQ);
+extern int maximum(priority_queue *PQ);
 
 /** Returns the value of the head (start) */
-int peek(priority_queue *PQ);
+extern int peek(priority_queue *PQ);
 
 #endif
