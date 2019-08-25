7a536c9051c1b2cb919b905ed1dd861d745e867d
anaferreira015 <dasilvaferreira.af@hotmail.com>
2019-08-25 19:56:02 -0300

    Altera a compressão

--- src/compress.c
+++ src/compress.c
@@ -56,9 +56,8 @@ void write_byte_to_file(FILE *compressedFile, int bytes[], int pos)
 
 void write_to_file(FILE *file, HASH_TABLE *hash_table, FILE *compressedFile)
 {
-    U_BYTE character;
-    int i, j = 0, size = 0;
-    U_BYTE byteFile = 0;
+    U_BYTE character, byteFile = 0;
+    int i, j = 0, size = 0, amount = 0, rest_size = 0;
     while (fscanf(file, "%c", &character) != EOF)
     {
         while(hash_table->matriz[character][j] != '\0') {
@@ -66,29 +65,35 @@ void write_to_file(FILE *file, HASH_TABLE *hash_table, FILE *compressedFile)
             j++;
         }
         j = 0;
-        printf("aqui %c %d\n", character, size);
-        for (i = 7; i >= 0; i--)
+        // printf("aqui %c %d\n", character, size);
+        for (i = size; i >= 0; i--)
         {
             if (hash_table->matriz[character][j] != '\0')
             {
                 if (hash_table->matriz[character][j] != '0')
                 {
                     byteFile = set_bit(byteFile, i);
+                    amount++;
                     j++;
                 }
                 else
                 {
+                    amount++;
                     j++;
                 }
             }
-            else
+            if(amount == 8) 
             {
-
+                fprintf(compressedFile, "%c", byteFile);
+                byteFile = 0;
+                amount = 0;
             }
         }
-        fprintf(compressedFile, "%c", byteFile);
-        byteFile = 0;
     }
+    // if(amount != 0) {
+    //     byteFile = set_bit(byteFile, i);
+    //     byteFile = 0;
+    // }
     int a[1];
     a[0] = byteFile;
     print_byte(a, 0);
